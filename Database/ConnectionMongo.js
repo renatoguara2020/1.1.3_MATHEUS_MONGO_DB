bdaca87fdf5d91157357f9b85325d50d544b48bd
Renato Alves Soares <renatoguara2020@gmail.com>
2022-05-13 16:16:29 -0300

    commit changes

--- Database/ConnectionMongo.js
+++ Database/ConnectionMongo.js
@@ -1,8 +1,12 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/mydb";
+var url = "mongodb://localhost:27017/";
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
-  console.log("Database created!");
-  db.close();
+  var dbo = db.db("testes_udemy");
+  dbo.createCollection("customers", function(err, res) {
+    if (err) throw err;
+    console.log("Collection created!");
+    db.close();
+  });
 });
